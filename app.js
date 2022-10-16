cb454027d0b5204b34c504a8e782d64e3eb83cd6
Greg <gdbabbert@gmail.com>
2022-10-16 17:08:13 -0400

    work on node file

---
+++ app.js
@@ -0,0 +1,15 @@
+const amount = 12
+
+if (amount < 10) {
+    console.log('small number')
+} else {
+    console.log('large number')
+}
+// directory name
+console.log('__dirname : ', __dirname)
+// file name
+console.log('__filename : ', __filename)
+// require function
+console.log('require : ', require)
+console.log('module : ', module)
+console.log('process : ', process)
