6acd8f74715c2225dd95e97b17103d035a95283b
Greg Babbert <gdbabbert@gmail.com>
2023-10-31 07:04:20 -0400

    made hello world for node

--- node-docs/index.js
+++ node-docs/index.js
@@ -1,1 +1,15 @@
 // This is for going over the Node.js learn tutorial, starting here: https://nodejs.org/en/learn
+
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// Run with  node node-docs/index.js
